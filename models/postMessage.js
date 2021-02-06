4e688d5a0d930dfe230f5a49c065fcf4c76425eb
TJ <tooraj@hotmail.com>
2021-02-06 14:33:38 -0800

    added postMessage.js creating postSchema and exporting postMessage

---
+++ models/postMessage.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const postSchema = Schema ({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [ String ],
+    likeCount: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    }
+});
+
+const PostMessage = mongoose.model('PostMessage', postSchema);
+
+module.export = postMessage;
