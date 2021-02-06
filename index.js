1b2601e1db4a692da516c5a376ceae161c6728cb
TJ <tooraj@hotmail.com>
2021-02-05 20:13:49 -0800

    imported express, bodyParser, mongoose, cors, connected to atlas cluster, running on local host

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb",extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb",extended: true}));
+app.use(cors());
+
+// need to move 
+const CONNECTION_URL = 'mongodb+srv://Ariozal:Ariozal123@cluster0.7tfkj.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, ()=> console.log('Server running on port: ${PORT}')))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
