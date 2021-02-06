7e0782dcacd6aa41ca4181b47c0c4859632baacc
TJ <tooraj@hotmail.com>
2021-02-06 12:58:58 -0800

    added routes and posts, added /posts to index.js and running on local host

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js'
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb",extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb",extended: true}));
@@ -15,7 +19,7 @@ const CONNECTION_URL = 'mongodb+srv://Ariozal:Ariozal123@cluster0.7tfkj.mongodb.
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, ()=> console.log('Server running on port: ${PORT}')))
+    .then(() => app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
