54c14eef5c6df939ab26ecea0347d4602c17bb55
TJ <tooraj@hotmail.com>
2021-02-10 15:52:48 -0800

    added logic for likPost in routes and controllers

--- controllers/posts.js
+++ controllers/posts.js
@@ -44,3 +44,13 @@ export const deletePost = async (req, res) => {
 
     res.json({ message: 'Post'})
 }
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('no post with that id');
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount +1}, {new: true });
+    res.json(updatedPost);
+}
